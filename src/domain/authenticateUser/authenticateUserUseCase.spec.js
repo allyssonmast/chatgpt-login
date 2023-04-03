b91835583b27e8a20668ba7b3c16b3cf611ad767
Allysson Mastrangelo <allysson-m@hotmail.com>
2023-04-03 15:44:28 -0300

    refactory usecase

--- src/domain/authenticateUser/authenticateUserUseCase.spec.js
+++ src/domain/authenticateUser/authenticateUserUseCase.spec.js
@@ -1,12 +1,11 @@
 
-
 describe('Authenticate User Use Case', () => {
-    test('Should return 401 if invalid email is provided', async () => {
-        const { sut, userRepositoryStub } = makeSut();
-        jest.spyOn(userRepositoryStub, 'findByEmail').mockReturnValueOnce(null);
+  test('Should return 401 if invalid email is provided', async () => {
+    const { sut, userRepositoryStub } = makeSut()
+        jest.spyOn(userRepositoryStub, 'findByEmail').mockReturnValueOnce(null)
 
-        const response = await sut.execute({ email: 'invalid_email@example.com', password: 'any_password' });
+        const response = await sut.execute({ email: 'invalid_email@example.com', password: 'any_password' })
 
-        expect(response.statusCode).toBe(401);
-    });
-});
+        expect(response.statusCode).toBe(401)
+    })
+})
